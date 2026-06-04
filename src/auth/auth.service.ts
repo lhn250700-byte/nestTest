e37fecdaea740771ca3d7bea28c30d4e87f10ab0
lhn250700-byte <lhn250700@gmail.com>
2026-06-04 21:44:01 +0900

    feat: login with jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,21 +2,33 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './auth-credential.dto';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.userRepository.create({ username, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.userRepository.create({
+      username,
+      password: hashedPassword,
+    });
     try {
       await this.userRepository.save(user);
     } catch (error) {
@@ -24,5 +36,19 @@ export class AuthService {
         throw new ConflictException('Existing username');
       else throw new InternalServerErrorException();
     }
+  }
+
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.userRepository.findOneBy({ username });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // 유저 토큰 생성 ( Secret + Payload )
+      const payload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken };
+    } else throw new UnauthorizedException('login failed');
   }
 }
